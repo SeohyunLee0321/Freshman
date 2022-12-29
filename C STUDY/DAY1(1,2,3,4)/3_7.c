6710070cb81c475d33df9934c5b2f1fe6e86aed5
3-21AM <117229525+SeohyunLee0321@users.noreply.github.com>
2022-12-30 07:40:25 +0900

    freshman c study

---
+++ C STUDY/DAY1(1,2,3,4)/3_7.c
@@ -0,0 +1,10 @@
+// Hello, world! 와 1234567890 이 각 줄에 출력
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, world!\n");
+	printf("123456789");
+
+	return 0;
+}
---
+++ C STUDY/DAY1(1,2,3,4)/3_9.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n%s", "Hello, world!", "Hello, world!");
+
+	return 0;
+}
